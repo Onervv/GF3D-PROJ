c34a308880213ce227814fd7cfc8aec77a373368
Onervv <cperrette123@gmail.com>
2025-11-25 10:58:22 -0500

    Modifyied const, phys as goood as its going to get

--- include/physics.h
+++ include/physics.h
@@ -5,12 +5,12 @@
 #include <math.h>
 
 // Tunable constants
-#define PHYS_GRAVITY            -0.1f
-#define PHYS_TERMINAL_VELOCITY  -10.0f
+#define PHYS_GRAVITY            -0.01f // ok
+#define PHYS_TERMINAL_VELOCITY  -20.0f
 #define PHYS_GROUND_FRICTION     0.96f
-#define PHYS_AIR_FRICTION        0.985f
-#define PHYS_MAX_AIR_SPEED       4.0f
-#define PHYS_SLOPE_SLIDE_FACTOR  0.05f
+#define PHYS_AIR_FRICTION        3.985f
+#define PHYS_MAX_AIR_SPEED       1.0f
+#define PHYS_SLOPE_SLIDE_FACTOR  0.03f
 
 void physics_apply(Entity *self, Uint8 onGround, Uint8 ignoreGravity);
 void physics_apply_slope(Entity *self, GFC_Vector3D groundNormal, Uint8 onGround);
